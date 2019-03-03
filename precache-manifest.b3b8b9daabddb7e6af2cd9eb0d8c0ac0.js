e603cca37ca562f39e965d8ec8840a3c432316f0
Vladik <optimus014@gmail.com>
2019-03-03 18:37:08 +0200

    Updates

---
+++ precache-manifest.b3b8b9daabddb7e6af2cd9eb0d8c0ac0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "08e37f32075bee95c42f",
+    "url": "/tabs-react/static/js/runtime~main.08e37f32.js"
+  },
+  {
+    "revision": "d1cebf993e060ab0930b",
+    "url": "/tabs-react/static/js/main.d1cebf99.chunk.js"
+  },
+  {
+    "revision": "c88c70e5f4ff8bea6fac",
+    "url": "/tabs-react/static/js/2.c88c70e5.chunk.js"
+  },
+  {
+    "revision": "d1cebf993e060ab0930b",
+    "url": "/tabs-react/static/css/main.d1d430fb.chunk.css"
+  },
+  {
+    "revision": "ad1ff51df86c5a926dc64b240287548e",
+    "url": "/tabs-react/index.html"
+  }
+];
